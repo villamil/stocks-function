182e589666b2f28fc458bca5305a425f1949b687
Luis Villamil <villamil_50@hotmail.com>
2022-04-27 10:18:01 -0500

    feat: add new dockerized app

---
+++ app.py
@@ -0,0 +1,26 @@
+import yfinance as yf
+import boto3
+import uuid
+import io
+
+def handler(event, context):
+    if "ticker" in event:
+        client = boto3.client('kinesis')
+
+        stockData = yf.download(event['ticker'], period="2y", interval="1h")
+
+        with io.StringIO() as csv_buffer:
+            stockData.to_csv(csv_buffer, index=False)
+            response = client.put_record(
+                StreamName='stocks',
+                Data=csv_buffer.getvalue(),
+                PartitionKey=str(uuid.uuid4())
+            )
+            return response
+    else:
+        return "Send a ticker"
+
+
+# docker build -t stocks-info . 
+# docker tag stocks-info:latest 670836484442.dkr.ecr.us-east-1.amazonaws.com/stocks-info:latest 
+# docker push 670836484442.dkr.ecr.us-east-1.amazonaws.com/stocks-info:latest 
