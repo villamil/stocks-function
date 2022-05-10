814edf334ef5db27f154dbfe6bf070934f352da7
Luis Villamil <villamil_50@hotmail.com>
2022-05-09 20:23:17 -0500

    add notebooks

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,9 @@
 import yfinance as yf
 import boto3
 import uuid
-import requests 
+import requests
 import os
+
 
 def lambda_handler(event, context):
     if "symbol" in event and "interval" in event:
@@ -10,7 +11,8 @@ def lambda_handler(event, context):
 
         apikey = os.environ['API_KEY']
 
-        req = requests.get('https://api.twelvedata.com/time_series?symbol={symbol}&interval={interval}&format=CSV&apikey={apikey}'.format(symbol=event['symbol'], interval=event['interval'], apikey=apikey ))
+        req = requests.get('https://api.twelvedata.com/time_series?symbol={symbol}&interval={interval}&format=CSV&apikey={apikey}'.format(
+            symbol=event['symbol'], interval=event['interval'], apikey=apikey))
         url_content = req.content
         response = client.put_record(
             StreamName='stocks',
@@ -20,8 +22,3 @@ def lambda_handler(event, context):
         return response
     else:
         return "Send a symbol and an interval"
-
-
-# docker build -t stocks-info . 
-# docker tag stocks-info:latest 670836484442.dkr.ecr.us-east-1.amazonaws.com/stocks-info:latest 
-# docker push 670836484442.dkr.ecr.us-east-1.amazonaws.com/stocks-info:latest 
